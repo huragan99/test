97ed448dcacc1950053b25f8afff27251822ee6a
Kamil <kamilek.ko@gmail.com>
2017-01-25 11:35:36 +0100

    25.01 First5Method/EveryNChars

---
+++ test/README.md
@@ -0,0 +1,5 @@
+# test
+This is testing repository.
+Changed line.
+That line was adding at January.
+That line was added at 25.01 10:25
---
+++ test/Warmup_1/First5Method/Main.java
@@ -0,0 +1,15 @@
+package codingbat5first;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//EveryNChars
+		EveryNChars testForENC = new EveryNChars();
+		String result = testForENC.everyNth("Kamil", 2);
+		System.out.println("kamil co 2 to: "+ result);
+		
+	}
+
+}
